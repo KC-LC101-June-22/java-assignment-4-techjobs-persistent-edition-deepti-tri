0386834dd9a309241b718c4f5f49105a270e9ed9
Deeps <100620410+deepti-tri@users.noreply.github.com>
2022-11-22 02:33:38 -0600

    added the code :rocket:

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,6 +21,17 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    EmployerRepository employerRepository;
+
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    SkillRepository skillRepository;
+
+
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -27,6 +44,8 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         return "add";
     }
 
@@ -38,6 +57,11 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+        newJob.setEmployer(employerRepository.findById(employerId).orElse(new Employer()));
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
+        jobRepository.save(newJob);
+
 
         return "redirect:";
     }
